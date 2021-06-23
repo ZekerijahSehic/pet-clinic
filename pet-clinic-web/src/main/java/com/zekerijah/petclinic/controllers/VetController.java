ffe8084ed385c5dc410ecb72eda3006c5680a45b
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-23 17:58:27 +0200

    Correct mapping anotation

--- pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/VetController.java
@@ -2,11 +2,12 @@ package com.zekerijah.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @GetMapping({"/vets", "vets/index", "vets/index.html"})
+    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
     public String listVets(){
         return "vets/index";
     }
