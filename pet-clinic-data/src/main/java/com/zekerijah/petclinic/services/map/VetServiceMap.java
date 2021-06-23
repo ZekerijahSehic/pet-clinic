7433e13635d852f63f83ba889651701c646e5a74
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-23 02:58:42 +0200

    Map based service

---
+++ pet-clinic-data/src/main/java/com/zekerijah/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.zekerijah.petclinic.services.map;
+
+import com.zekerijah.petclinic.model.Pet;
+import com.zekerijah.petclinic.model.Vet;
+import com.zekerijah.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
