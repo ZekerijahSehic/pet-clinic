5a394f2a05c3b84631bdfc617ad43a21b6383331
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-25 00:25:48 +0200

    VetControler & template

--- pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/VetController.java
@@ -1,14 +1,27 @@
 package com.zekerijah.petclinic.controllers;
 
+import com.zekerijah.petclinic.model.Vet;
+import com.zekerijah.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
         return "vets/index";
     }
+
 }
