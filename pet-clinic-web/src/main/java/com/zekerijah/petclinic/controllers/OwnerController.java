6fa6f2a4480aefc809bd39f1585046ae2f0f22e5
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-24 23:58:10 +0200

    OwnerController & template

--- pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/zekerijah/petclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package com.zekerijah.petclinic.controllers;
 
+import com.zekerijah.petclinic.model.Owner;
+import com.zekerijah.petclinic.services.OwnerService;
+import com.zekerijah.petclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "owners/index", "owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
