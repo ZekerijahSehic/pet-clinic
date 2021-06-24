fc32205698a5ece1ed0a55097d0310b76a40643c
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-24 23:55:13 +0200

    Add @Service to ...ServiceMap

--- pet-clinic-data/src/main/java/com/zekerijah/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/zekerijah/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.zekerijah.petclinic.services.map;
 
 import com.zekerijah.petclinic.model.Pet;
 import com.zekerijah.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
