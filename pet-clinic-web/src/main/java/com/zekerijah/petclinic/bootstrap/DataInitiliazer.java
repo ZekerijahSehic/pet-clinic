6ab6dbfa3baa54d3b138edad43abf04e6356a5e6
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-24 23:59:23 +0200

    Fix service

--- pet-clinic-web/src/main/java/com/zekerijah/petclinic/bootstrap/DataInitiliazer.java
+++ pet-clinic-web/src/main/java/com/zekerijah/petclinic/bootstrap/DataInitiliazer.java
@@ -1,11 +1,7 @@
 package com.zekerijah.petclinic.bootstrap;
 
 import com.zekerijah.petclinic.model.Owner;
-import com.zekerijah.petclinic.model.Vet;
 import com.zekerijah.petclinic.services.OwnerService;
-import com.zekerijah.petclinic.services.VetService;
-import com.zekerijah.petclinic.services.map.OwnerServiceMap;
-import com.zekerijah.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,49 +9,29 @@ import org.springframework.stereotype.Component;
 public class DataInitiliazer implements CommandLineRunner {
 
     private final OwnerService ownerService;
-    private final VetService vetService;
 
-
-    public DataInitiliazer(OwnerService ownerService, VetService vetService) {
+    public DataInitiliazer(OwnerService ownerService) {
         this.ownerService = ownerService;
-        this.vetService = vetService;
-    }
-
-    public DataInitiliazer() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
+        owner1.setId(1L);
         owner1.setFirstName("Jon");
         owner1.setLastName("Doe");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
+        owner2.setId(2L);
         owner2.setFirstName("Jane");
         owner2.setLastName("Doe");
 
         ownerService.save(owner2);
 
         System.out.println("Owners: ");
-
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Billy");
-        vet1.setLastName("Bens");
-
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Jessie");
-        vet2.setLastName("Park");
-
-        vetService.save(vet2);
-
-        System.out.println("Vets: ");
 
     }
 }
